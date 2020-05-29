b5d29aa4d2021e2fefb24c3da7ba15753f6fc7fe
nataliac29 <calvonatalia1@gmail.com>
2020-05-29 12:41:39 -0400

    Initial commit

---
+++ src/lib/context.js
@@ -0,0 +1,44 @@
+const { isSameHour } = require('date-fns')
+const { createToken, decodeToken } = require('./auth')
+const User = require('../models/User')
+const { generateLoaders } = require('./dataloader')
+
+// The method exported here sets the context for all resolvers and refreshes tokens
+module.exports = async ({ req, res }) => {
+  const context = { req, res, loaders: generateLoaders() }
+  // If login or register, skip auth requirements
+  if (req.body.operationName === 'login' || req.body.operationName === 'register') {
+    return context
+  }
+
+  // Collect JWT, escape 'Bearer' prefix
+  const jwt = req.headers.authorization ? req.headers.authorization.slice(7) : null
+
+  if (!jwt) {
+    // No JWT present for auth
+    return context
+  }
+
+  try {
+    const {
+      sub, iat,
+    } = decodeToken(jwt)
+
+    const user = await User.query().findById(sub)
+    context.user = user
+    if (isSameHour(iat, new Date().getTime() / 1000)) {
+      return context
+    }
+
+    // If token is more than an hour old, refresh it
+    const payload = {
+      sub: user.id,
+    }
+    res.set('x-token', createToken(payload))
+
+    return context
+    // If failed context creation, make unathenticated request
+  } catch (error) {
+    return context
+  }
+}
